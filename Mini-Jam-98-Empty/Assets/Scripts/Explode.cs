64b974acc08cf8249b18ba3e0b1226ab04de273d
wluxie <49565505+wluxie@users.noreply.github.com>
2022-01-23 01:01:58 -0800

    Added explosion upon collision

--- Mini-Jam-98-Empty/Assets/Scripts/Explode.cs
+++ Mini-Jam-98-Empty/Assets/Scripts/Explode.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
-    private int cubesPerAxis;
-    private float delay = 1f;
-    private float explosionForce = 100f;
-    private float radius = 2f;
-    
-    void Start()
+    public int cubesPerAxis = 3;
+    public float explosionForce = 1000f;
+    public float radius = 20f;
+
+    private void OnTriggerEnter(Collider other) 
     {
-        cubesPerAxis = Random.Range(3,6);
+        ExplodeCube();
     }
 
-    private void ExplodeCube()
+    public void ExplodeCube()
     {
-        
+        for (int x = 0; x < cubesPerAxis; x++)
+        {
+            for (int y = 0; y < cubesPerAxis; y++)
+            {
+                for (int z = 0; z < cubesPerAxis; z++)
+                {
+                    CreateDebris(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    public void CreateDebris(Vector3 croods)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Renderer rend = cube.GetComponent<Renderer>();
+        rend.material = GetComponent<Renderer>().material;
+
+        cube.transform.localScale = transform.localScale / cubesPerAxis;
+        Vector3 firstCube = transform.position - transform.localScale / 2 + cube.transform.localScale / 2;
+        cube.transform.position = firstCube + Vector3.Scale(croods, cube.transform.localScale);
+
+        Rigidbody rb = cube.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.mass = .5f;
+        rb.AddExplosionForce(explosionForce, transform.position, radius, 3.0f, ForceMode.Force);
+
+        //==============================================================
+        // Dunno how to despawn debris but its probably fine ¯\_(ツ)_/¯
+        // p.s I tried :(
+        //==============================================================
     }
 }
